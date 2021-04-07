222a368ac1b34cb503a50c149348fb3bad203ecd
Sayak China <sayakchina031001@gmail.com>
2021-04-07 21:55:42 +0530

    eChanges made

--- script.js
+++ script.js
@@ -18,20 +18,41 @@ $(document).ready(function() {
     $("#eq").click(function() {
         s=$("#screen").val();
         let s1=s.replace("%", "*0.01*");
-        let ans = eval(s1);
-        $("#screen").val(ans);
+        let s2=s1.replace("undefined","");
+        let ans = eval(s2);
+        if(ans.search("undefined")===-1)
+        {
+          $("#screen").val(ans);
         s = ans;
+      }
     });
 });
+$(document).keydown(function(e) { if (e.keyCode == 8) e.preventDefault(); });
+ $(document).keyup(function(e){
+   if(e.keyCode == 8)
+   {
+     s=$("#screen").val();
+     $("#screen").val(s.substring(0, s.length-1));
+     s=$("#screen").val();
+   }
+
+ });
+
 
 $(document).keypress(function(e) {
+
+    if(isNumberKey(e))
+    $("#screen").focus();
     if(e.key=="Enter") {
         s=$("#screen").val();
         let s1=s.replace("%", "*0.01*");
-        let ans = eval(s1);
-        $("#screen").val(ans);
+        let s2=s1.replace("undefined","");
+        let ans = eval(s2);
+                  $("#screen").val(ans);
         s = ans;
+
     }
+
 });
 
 function isNumberKey(evt){
@@ -39,7 +60,8 @@ function isNumberKey(evt){
     if (charCode == 37 || charCode == 42 || charCode == 43 || charCode == 45 || charCode == 46 || charCode == 47)
     {
         s+=evt.key;
-        $("#screen").val(s);
+        // console.log(s);
+        // $("#screen").val(s);
         return true;
     }
     else if (charCode > 31 && (charCode < 48 || charCode > 57))
@@ -47,7 +69,8 @@ function isNumberKey(evt){
     else
     {
         s+=evt.key;
-        $("#screen").val(s);
+        // console.log(s);
+        // $("#screen").val(s);
         return true;
     }
 }
